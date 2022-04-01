16e7c7795223c6f9ad83c083997810e7027fddcc
keithkinki <92848946+keithkinki@users.noreply.github.com>
2022-04-01 14:09:42 +0800

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -30,7 +30,7 @@ exports.execute = async (req, res) => {
     await SFClient.saveData(process.env.DATA_EXTENSION_EXTERNAL_KEY, [
       {
         keys: {
-          Id: id,
+          id: id,
           SubscriberKey: data.inArguments[0].contactKey,
         },
         values: {
