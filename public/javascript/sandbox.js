808c94b50581a28379625a020b5d7de8374cef7e
keithkinki <92848946+keithkinki@users.noreply.github.com>
2022-04-02 20:07:19 +0800

    Update sandbox.js

--- public/javascript/sandbox.js
+++ public/javascript/sandbox.js
@@ -1,1 +1,8 @@
 console.log('kn1:');
+console.log(document.referrer);
+var referrerJB = 'https://jbinteractions.s4.marketingcloudapps.com/'
+console.log(referrerJB);
+
+if (document.referrer === referrerJB) {
+  console.log('yes'); 
+}
