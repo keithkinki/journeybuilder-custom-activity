674f80208caefb96d4470647cddd8f72c53e3a4a
keithkinki <92848946+keithkinki@users.noreply.github.com>
2022-04-02 11:25:24 +0800

    Update customActivity.js

--- public/javascript/customActivity.js
+++ public/javascript/customActivity.js
@@ -60,18 +60,15 @@ function onClickedNext() {
   if (currentStep.key === "step3") {
     save();
   } else {
-    console.log('kn1: onclicknext()');
     connection.trigger("nextStep");
   }
 }
 
 function onClickedBack() {
-    console.log('kn1: onclickback()');
   connection.trigger("prevStep");
 }
 
 function onGotoStep(step) {
-    console.log('kn1: ongotostep()');
   showStep(step);
   connection.trigger("ready");
 }
