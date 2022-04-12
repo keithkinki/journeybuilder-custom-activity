e96554617f84a1c0dcbdc18cbfbe63ce98d6af29
keithkinki <92848946+keithkinki@users.noreply.github.com>
2022-04-12 18:06:47 +0800

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -25,6 +25,7 @@ exports.execute = async (req, res) => {
   logger.info('a1');
   logger.info('bb-'+ data.inArguments[0].myInArgument) + 'cc';
   logger.info('b1');
+  logger.info('cc-' + data.inArguments[0];
   
   logger.info(data);
 
